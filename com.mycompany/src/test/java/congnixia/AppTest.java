3725f67e4c924aa4cb0611a8b231925ab96f2777
schalla <schalla@adobe.com>
2022-02-21 14:45:07 -0700

    Adding stuff

---
+++ com.mycompany/src/test/java/congnixia/AppTest.java
@@ -0,0 +1,20 @@
+package congnixia;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
